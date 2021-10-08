349c40992911a1011bd8cf1f8feddc796f8d6f73
Leonardo Herrera <leo2399@gmail.com>
2021-10-08 03:27:59 -0400

    first commit

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Leonardo Herrera
+ */
+
+package baseline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution41 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner inputFile = new Scanner(new File("exercise41_input.txt"));
+        // read the input file
+        // use a function to read the names from the file
+        // store names in an array list
+        // sort the names in alphabetical order
+        // call the writeOutput() function
+        // close the input file
+
+    }
+
+    private void readNames(){
+        // use this function to read the names from the input file
+        // go through the file and read each name
+    }
+
+    private void writeOutput(){
+        //use this function to write the output txt file
+
+        // create the output file to write into it
+        // have the number of names in the files read
+        // write the output file
+        // close the output file
+    }
+}
