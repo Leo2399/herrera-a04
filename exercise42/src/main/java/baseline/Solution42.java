7f423135495fb3ff77bb122e5dd03baeb4f6da13
Leonardo Herrera <leo2399@gmail.com>
2021-10-13 02:43:31 -0400

    intitial commit

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args) {
+
+        fileReader read = new fileReader();
+        // Use the fileReader class to read form the input file
+        // write the output file after the data is read from the input file from the data stored in the array list
+
+    }
+}
---
+++ exercise42/src/main/java/baseline/fileReader.java
@@ -0,0 +1,19 @@
+package baseline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class fileReader {
+
+    private ArrayList<String> parseData() throws FileNotFoundException {
+        Scanner inputFile = new Scanner(new File("data/exercise42_input.txt"));
+        // Open file and read the data
+        // Store data in an array list
+
+       ArrayList<String> data = new ArrayList<>();
+
+        return data;
+    }
+}
