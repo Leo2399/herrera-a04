fb0d4538258dd110ed6f90c7f2d9897b195f278d
Leonardo Herrera <leo2399@gmail.com>
2021-10-13 02:07:41 -0400

    second commit

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -5,34 +5,57 @@
 
 package baseline;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Solution41 {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner inputFile = new Scanner(new File("exercise41_input.txt"));
+    public static void main(String[] args) throws IOException {
+
+        String input = "data/exercise41_input.txt";
+        Scanner inputFile = new Scanner(new File(input));
         // read the input file
-        // use a function to read the names from the file
         // store names in an array list
+        ArrayList<String> names = new ArrayList<>();
+
+        // call readNames() function
+        readNames(inputFile,names);
+
         // sort the names in alphabetical order
+        Collections.sort(names);
+
         // call the writeOutput() function
+        writeOutput(names);
+
         // close the input file
+        inputFile.close();
 
     }
 
-    private void readNames(){
+     public static void readNames(Scanner in, ArrayList<String> names){
         // use this function to read the names from the input file
         // go through the file and read each name
+        while(in.hasNextLine()){
+            names.add(in.nextLine());
+        }
     }
 
-    private void writeOutput(){
+     public static void writeOutput(ArrayList<String> names) throws IOException {
         //use this function to write the output txt file
-
+        String output = "data/exercise41_output.txt";
         // create the output file to write into it
+        FileWriter outputFile = new FileWriter(output);
         // have the number of names in the files read
+
+        outputFile.write("Total of "+names.size()+" names\n");
+
         // write the output file
+        for(String i : names){
+            outputFile.write(i + "\n");
+        }
         // close the output file
+        outputFile.close();
     }
 }
