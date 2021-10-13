14d0f6fc078bc8e54a36267ba95b83f70c897a4a
Leonardo Herrera <leo2399@gmail.com>
2021-10-13 17:48:13 -0400

    added code and got it to work

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -1,11 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Leonardo Herrera
+ */
+
 package baseline;
 
+import java.io.IOException;
+
 public class Solution42 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         fileReader read = new fileReader();
         // Use the fileReader class to read form the input file
-        // write the output file after the data is read from the input file from the data stored in the array list
 
+        // Set the headers for the output
+        System.out.printf("%-10s%-10s%-10s%n", "Last", "First", "Salary");
+        System.out.println("--------------------------");
+
+        // Get the data from the txt file though the fileReader class
+        read.parseData();
     }
 }
