2e1caf3488a3749448f7d86f80a878de421e4a10
Leonardo Herrera <leo2399@gmail.com>
2021-10-14 18:18:13 -0400

    second commit

--- exercise43/src/main/java/baseline/SiteGenerator.java
+++ exercise43/src/main/java/baseline/SiteGenerator.java
@@ -1,31 +1,59 @@
 package baseline;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class SiteGenerator {
-    public void createSite(){
+
+    String siteName;
+    String author;
+    String path;
+
+    public File createSite(){
         // use this method to create the website folder
         // create the folder by creating a file
         // the name of the folder should be the name of the website
+        File site = new File(path+siteName);
+
         // create the directory
+        site.mkdirs();
+        return site;
     }
 
-    public void createJSFolder(){
+    public File createJSFolder(){
         // this method will create the JavaScript folder
         // create a new file called "/js"
         // add "/js" to the directory along with the name of the website
+        File js = new File(path+siteName+"/js");
+
+        js.mkdirs();
+        return js;
     }
 
-    public void createCSSFolder(){
+    public File createCSSFolder(){
         // this method will create the CSS folder
         // create new file called "/css"
         // add "/css" to the directory along with the name of the website
+        File css = new File(path+siteName+"/css");
 
+        css.mkdirs();
+        return css;
     }
 
-    public void createHTML(){
+    public File createHTML() throws IOException {
         // this method creates the HTML file
         // store the name of the website in <title> and the author in <meta> tags
         // create new file called "/index.html"
+        File html = new File(path+siteName+"/index.html");
+        String htmlFile = "<title>" +siteName+ "</title>\n<meta>"+author+"</meta>";
+
         // use the file writer to create a new output file
+        FileWriter output = new FileWriter(html);
+        output.write(htmlFile);
+
         // close the output file
+        output.close();
+        return html;
     }
 }
--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,14 +1,51 @@
 package baseline;
 
-public class Solution43 {
-    public static void main(String[] args) {
-        // prompt for the name of the website.
-        // prompt for the author.
-        // ask if they want to add JavaScript.
-        // ask if they want to add CSS.
-        // generate the website with the html file.
-        // if "y", create the js folder
-        // if "y", create the css folder
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Solution43 {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    // Using this public string to
+    public static final String Created = "Created ";
+
+    public static void main(String[] args) throws IOException {
+
+        SiteGenerator website = new SiteGenerator();
+
+        // prompt for the name of the website.
+        System.out.print("Site name: ");
+        String site = in.nextLine();
+
+        // prompt for the author.
+        System.out.print("Author: ");
+        String name = in.nextLine();
+
+        // ask if they want to add JavaScript.
+        System.out.print("Do you want a folder for JavaScript? ");
+        String js = in.nextLine();
+
+        // ask if they want to add CSS.
+        System.out.print("Do you want a folder for CSS? ");
+        String css = in.nextLine();
+
+        website.siteName = site;
+        website.author = name;
+        website.path = "data/";
+
+        // generate the website with the html file.
+        System.out.println(Created+website.createSite());
+        System.out.println(Created+website.createHTML());
+
+        // if "y", create the js folder
+        if(js.equals("y")||js.equals("Y")){
+            System.out.println(Created+website.createJSFolder());
+        }
+
+        // if "y", create the css folder
+        if(css.equals("y")||css.equals("Y")){
+            System.out.println(Created+website.createCSSFolder());
+        }
     }
 }
