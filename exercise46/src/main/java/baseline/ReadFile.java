c15a1881c8e2fd330d86da41c07427b50ec620c4
Leonardo Herrera <leo2399@gmail.com>
2021-10-17 18:20:38 -0400

    updated ReadFile

--- exercise46/src/main/java/baseline/ReadFile.java
+++ exercise46/src/main/java/baseline/ReadFile.java
@@ -1,7 +1,9 @@
 package baseline;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ReadFile {
@@ -9,17 +11,36 @@ public class ReadFile {
     // set instance variables
     private Scanner in;
     private File inputFile;
-    private HashMap<String,Integer> words = new HashMap<>();
+    private Map<String,Integer> words = new HashMap<>();
 
-    public void readFile(){
+    public File readFile() throws FileNotFoundException {
         // open the file and use the scanner to read through it
+        inputFile = new File("data/exercise46_input.txt");
+        in = new Scanner(inputFile);
+
         // store the words in the file into the hashmap
+        while(in.hasNext()){
+            String input = in.nextLine();
+            String[] arr = input.split(" ");
+
+            for (String key : arr) {
+                if (words.containsKey(key)) {
+                    int value = words.get(key);
+                    value++;
+                    words.put(key, value);
+                } else {
+                    words.put(key, 1);
+                }
+            }
+        }
+        return inputFile;
         // use an array to store the names
         // count the repeated words and add its value to the hashmap
         // close the file
     }
 
-    public HashMap<String,Integer> getWords(){
+    public Map<String,Integer> getWords(){
         // return the hashmap
+        return words;
     }
 }
