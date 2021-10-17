5655692e96e4977e1dabf988caf860c2471c4621
Leonardo Herrera <leo2399@gmail.com>
2021-10-17 18:20:01 -0400

    second commit

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -1,11 +1,23 @@
 package baseline;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution46 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // create an object of ReadFile
+        ReadFile read = new ReadFile();
+        read.readFile();
+
         // read the file
         // get the hashmap
+        Map<String, Integer> words = read.getWords();
+
         // create an object of PrintFrequency
+        PrintFrequency print = new PrintFrequency(words);
+
         // display the results
+        print.display();
     }
 }
