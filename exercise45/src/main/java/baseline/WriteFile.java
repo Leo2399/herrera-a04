8cebfeee390c91eefaa693e6cea98f544cc6acc9
Leonardo Herrera <leo2399@gmail.com>
2021-10-17 02:35:46 -0400

    second commit

--- exercise45/src/main/java/baseline/WriteFile.java
+++ exercise45/src/main/java/baseline/WriteFile.java
@@ -5,6 +5,7 @@ package baseline;
  */
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class WriteFile {
@@ -15,13 +16,25 @@ public class WriteFile {
     FileWriter output;
     ArrayList<String> content;
 
-    public WriteFile(String fileName, ArrayList<String> content){
+    public WriteFile(String fileName, ArrayList<String> content) throws IOException {
         // create the output file with the name given by the user
+        output = new FileWriter("data/"+fileName);
+        this.fileName = fileName;
+        this.content = content;
     }
 
-    public void writeToFile(){
+    public void writeToFile() throws IOException {
         // for every instance of the word "utilize" and replace it with "use"
-        // write the new file
+        for(int i=0; i<content.size(); i++){
+            String replace = content.get(i);
+
+            replace = replace.replaceAll("utilize", "use");
+
+            // write the new file
+            output.write(replace+"\n");
+        }
+
         // close the file
+        output.close();
     }
 }
