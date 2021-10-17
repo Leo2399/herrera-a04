b6b83e4aabd2eddef73c5c63e0c57da1f2191ff2
Leonardo Herrera <leo2399@gmail.com>
2021-10-17 02:35:11 -0400

    second commit

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -1,13 +1,24 @@
 package baseline;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution45 {
     private static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ReadFile read = new ReadFile();
         // read the file
-        // ask for the name of the output file
+        read.read();
 
+        ArrayList<String> content = read.content();
+
+        // ask for the name of the output file
+        System.out.println("What is the name of the output file? ");
+        String name = in.nextLine();
+
+        WriteFile write = new WriteFile(name, content);
+        write.writeToFile();
     }
 }
