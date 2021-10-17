2660b5b1933a3c8efeed5da7415f44f40cb945a1
Leonardo Herrera <leo2399@gmail.com>
2021-10-17 02:34:36 -0400

    second commit

--- exercise45/src/main/java/baseline/ReadFile.java
+++ exercise45/src/main/java/baseline/ReadFile.java
@@ -1,21 +1,32 @@
 package baseline;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
     Use this class to read through the file
  */
 public class ReadFile {
+    ArrayList<String> data = new ArrayList<>();
 
-    public File read(){
+    public File read() throws IOException {
         // open the file to read from
         // use scanner to read through the file
+        File inputFile = new File("data/exercise45_input.txt");
+        Scanner in = new Scanner(inputFile);
+
         // store the contents of the file in an array
+        while (in.hasNextLine()){
+            data.add(in.nextLine());
+        }
+
+        return inputFile;
     }
 
     public ArrayList<String> content(){
         // return file content
+        return data;
     }
 
 }
